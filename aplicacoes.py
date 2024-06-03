89a00d02595bdd1a9fef3d43bbcbc04a92f507a1
PedroLemosBonini <pedrolemosbonini@gmail.com>
2024-06-03 16:17:56 -0300

    aplicacoes e modulo04

--- aplicacoes.py
+++ aplicacoes.py
@@ -44,5 +44,48 @@ if achado:
 else:
     print("Não encontrado!!!")
 
+#conferidor de aposta em loteria
+sorteados = [5, 11, 9, 42, 3, 49]
+apostados = [3, 7, 11, 42, 34, 49]
+acertos = 0
+
+for numero in sorteados:
+    if numero in apostados:
+        acertos += 1
+
+print(acertos)
+
+#remoção de números repetidos em uma lista
+lista = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+print("Lista original:", lista)
+
+#Lista de apoio
+vistos = []
+
+#Interar pela lista original de trás para frente
+for i in range(len(lista) -1, -1, -1):
+    #Se o número já está na lista "vistos", removê-lo da lista original
+    if lista[i] in vistos:
+        del lista[i]
+    else:
+        #Caso contrário, adicionar à lista "vistos"
+        vistos.append(lista[i])
+
+#Exibir a lista original modificada
+print("Lista Modificada:", lista)
+
+
+#Listas avançadas
+#2D - Listas aninhadas bidimensionais
+tabela = [[":(", ":)", ":|", ";-;"], [";-;", ":|", ":)", ":("], [":|", ":)", ";-;", ":("]]
+print(tabela[0][3])
+print(tabela)
+
+#3D - Matriz Tridimensional
+cubo = [[[":(", "y", "z"], [":)", "y", "z"], [":|", "y", "z"]], [["amor", "odio", "caridade"], ["paz", "esperança", "férias"], ["tina", "pior", "pp"]], [["restinga", "patrocinio", "rifaina"], ["Amazonas", "Fluminense", "Santos"], ["pizza", "lasanha", "pastel"]]]
+print(cubo)
+print(cubo[1])
+print(cubo[1][0])
+print(cubo[1][0][2])
 
 time.sleep(2)
