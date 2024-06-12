b0d7d7e8d9de316285358c2be63cd63302a61e59
PedroLemosBonini <pedrolemosbonini@gmail.com>
2024-06-12 10:45:21 -0300

    Tuplas 1

---
+++ funcoes_12-06.py
@@ -0,0 +1,29 @@
+import time
+
+# def hi_everybody(my_list):
+#     for name in my_list:
+#         print("Oi,", name)
+ 
+# hi_everybody(["Tina", "Gabi", "João"])
+
+def criar_lista(n):
+    lista = []
+    for i in range(n):
+        lista.append(i)
+    return lista
+
+print(criar_lista(10))
+
+global a 
+a = 1
+ 
+def fun():
+    a = 2
+    print(a)
+ 
+ 
+a = 3
+fun()
+print(a)
+
+time.sleep(3)
